05bc87938f241bf460c461abd68f88717c6df2e6
Евгений <evolchokhot@gmail.com>
2024-12-21 22:33:53 +0300

    main
    Grade 1. Этап 1: Задание 1

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Евгений"
+title = "Заметка 1"
+content = "Первая заметка созданная в этом проекте"
+status = "active"
+created_date = "21-12-2024"
+issue_date = "21-12-2034"
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
