0865e0c15e91ba330b5992db04218dee70c953c5
Евгений <evolchokhot@gmail.com>
2025-01-27 14:09:20 +0300

    Update add_titles_loop.py

--- Stage-2/add_titles_loop.py
+++ Stage-2/add_titles_loop.py
@@ -1,9 +1,10 @@
 title_list = []
+def titles():
+    while True:
+        title = input("Добавьте заголовок заметки (Чтобы закончить добавление нажмите Enter): ")
+        if  title != "":
+            title_list.append(title)
+        else: break
 
-while True:
-    title = input("Добавьте заголовок заметки (Чтобы закончить добавление нажмите Enter): ")
-    if  title != "":
-        title_list.append(title)
-    else: break
-
+titles()
 print(f'Заголовки вашей заметки: {title_list}')
